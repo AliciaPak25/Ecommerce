1ae42ec2534de89ed48190acf592d659eb4cf677
Alicia <aliciappak@gmail.com>
2023-10-26 14:19:38 -0300

    connected to mongodb database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -7,4 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(6000, () => console.log("SERVER STARTED"));
+const PORT = process.env.PORT || 6000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//app.listen(6000, () => console.log("SERVER STARTED"));
